0fc7c2e6dbe1e80a82a88bfb71fcf973ea65e0fb
imiyu <imiyu@qq.com>
2014-06-15 10:23:38 +0800

    Signed-off-by: imiyu <imiyu@qq.com>

---
+++ pgrun/pgrun/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace pgrun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnPgbin = new System.Windows.Forms.Button();
+            this.txtPgbin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCmd = new System.Windows.Forms.TextBox();
+            this.txtPgdata = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnPgdata = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRuncmd = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cmbCmd = new System.Windows.Forms.ComboBox();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPgbin
+            // 
+            this.btnPgbin.Location = new System.Drawing.Point(322, 14);
+            this.btnPgbin.Name = "btnPgbin";
+            this.btnPgbin.Size = new System.Drawing.Size(57, 23);
+            this.btnPgbin.TabIndex = 0;
+            this.btnPgbin.Text = "选择";
+            this.btnPgbin.UseVisualStyleBackColor = true;
+            this.btnPgbin.Click += new System.EventHandler(this.btnPgbin_Click);
+            // 
+            // txtPgbin
+            // 
+            this.txtPgbin.Location = new System.Drawing.Point(65, 16);
+            this.txtPgbin.Name = "txtPgbin";
+            this.txtPgbin.Size = new System.Drawing.Size(241, 21);
+            this.txtPgbin.TabIndex = 1;
+            this.txtPgbin.TextChanged += new System.EventHandler(this.txtPgbin_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PG/bin：";
+            // 
+            // txtCmd
+            // 
+            this.txtCmd.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtCmd.Location = new System.Drawing.Point(3, 44);
+            this.txtCmd.Multiline = true;
+            this.txtCmd.Name = "txtCmd";
+            this.txtCmd.Size = new System.Drawing.Size(400, 98);
+            this.txtCmd.TabIndex = 3;
+            // 
+            // txtPgdata
+            // 
+            this.txtPgdata.Location = new System.Drawing.Point(65, 54);
+            this.txtPgdata.Name = "txtPgdata";
+            this.txtPgdata.Size = new System.Drawing.Size(241, 21);
+            this.txtPgdata.TabIndex = 4;
+            this.txtPgdata.TextChanged += new System.EventHandler(this.txtPgdata_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PG/data：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnPgdata);
+            this.groupBox1.Controls.Add(this.btnPgbin);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPgbin);
+            this.groupBox1.Controls.Add(this.txtPgdata);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 92);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "选择目录";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnPgdata
+            // 
+            this.btnPgdata.Location = new System.Drawing.Point(322, 52);
+            this.btnPgdata.Name = "btnPgdata";
+            this.btnPgdata.Size = new System.Drawing.Size(57, 23);
+            this.btnPgdata.TabIndex = 0;
+            this.btnPgdata.Text = "选择";
+            this.btnPgdata.UseVisualStyleBackColor = true;
+            this.btnPgdata.Click += new System.EventHandler(this.btnPgdata_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbCmd);
+            this.groupBox2.Controls.Add(this.btnStop);
+            this.groupBox2.Controls.Add(this.btnRun);
+            this.groupBox2.Controls.Add(this.txtCmd);
+            this.groupBox2.Location = new System.Drawing.Point(11, 110);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(406, 145);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "产生命令";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(111, 15);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(61, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(23, 15);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(61, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "运行";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(430, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(44, 17);
+            this.toolStripStatusLabel1.Text = "状态：";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtResult);
+            this.groupBox3.Controls.Add(this.btnClear);
+            this.groupBox3.Controls.Add(this.btnRuncmd);
+            this.groupBox3.Location = new System.Drawing.Point(12, 261);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(408, 142);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "执行/结果";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(322, 0);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(57, 33);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnRuncmd
+            // 
+            this.btnRuncmd.Location = new System.Drawing.Point(130, 0);
+            this.btnRuncmd.Name = "btnRuncmd";
+            this.btnRuncmd.Size = new System.Drawing.Size(103, 33);
+            this.btnRuncmd.TabIndex = 1;
+            this.btnRuncmd.Text = "RUN";
+            this.btnRuncmd.UseVisualStyleBackColor = true;
+            this.btnRuncmd.Click += new System.EventHandler(this.btnRuncmd_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cmbCmd
+            // 
+            this.cmbCmd.FormattingEnabled = true;
+            this.cmbCmd.Items.AddRange(new object[] {
+            "help",
+            "Init",
+            "regServer",
+            "unregServer"});
+            this.cmbCmd.Location = new System.Drawing.Point(259, 17);
+            this.cmbCmd.Name = "cmbCmd";
+            this.cmbCmd.Size = new System.Drawing.Size(121, 20);
+            this.cmbCmd.TabIndex = 7;
+            this.cmbCmd.SelectedIndexChanged += new System.EventHandler(this.cmbCmd_SelectedIndexChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtResult.Location = new System.Drawing.Point(3, 39);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(402, 100);
+            this.txtResult.TabIndex = 3;
+            this.txtResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 441);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "PG-RUN";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnPgbin;
+        private System.Windows.Forms.TextBox txtPgbin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCmd;
+        private System.Windows.Forms.TextBox txtPgdata;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnPgdata;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnRuncmd;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ComboBox cmbCmd;
+        private System.Windows.Forms.RichTextBox txtResult;
+    }
+}
+
---
+++ pgrun/pgrun/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+//using System.Linq;
+using System.Text;
+using System.Threading;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace pgrun
+{
+    public partial class Form1 : Form
+    {
+        string result="";
+        Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPgbin_Click(object sender, EventArgs e)
+        {
+            getpath(txtPgbin);
+        }
+        public void getpath(TextBox tb)
+        {
+            folderBrowserDialog1.ShowDialog();
+            if (String.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+                return;
+            tb.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnPgdata_Click(object sender, EventArgs e)
+        {
+            getpath(txtPgdata);
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtPgdata.Text)) return;
+            txtCmd.Text = "";
+            //pg_ctl start -W -D "%CD%/pgsql/data" -l "%CD%/pgsql/logfile" 
+            txtCmd.Text = String.Format("pg_ctl start -D \"{0}\"", txtPgdata.Text);
+        }
+
+        private void btnRuncmd_Click(object sender, EventArgs e)
+        {
+            if (!txtCmd.Text.StartsWith("pg_ctl "))
+            {
+                MessageBox.Show("命令错误！");
+                return;
+            }
+            Thread t1 = new Thread(new ThreadStart(runcmd));
+            t1.Start();
+            //runcmd("");
+        }
+        public void runcmd()
+        {
+            try
+            {
+                string exe = txtPgbin.Text + "\\pg_ctl.exe";
+                if (!File.Exists(exe))
+                {
+                    throw new ApplicationException("Can not find: " + exe);
+                }
+                StringBuilder sb = new StringBuilder();
+                sb.Append(txtCmd.Text.Substring(6));
+                //sb.Append(" yr");
+                //textBox2.AppendText(sb.ToString() + "\r\n");
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = exe;
+                proc.StartInfo.Arguments = sb.ToString();
+                proc.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start();
+                string output = proc.StandardOutput.ReadToEnd() + "\r\n" + proc.StandardError.ReadToEnd();
+                if (String.IsNullOrEmpty(output) == false)
+                    result += output+"\n";
+                    //MessageBox.Show(output);
+                //    txtResult.AppendText(output + "\r\n");
+                proc.WaitForExit();
+                //proc.BeginOutputReadLine();
+                proc.Close();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtResult.Text = result;
+            if (!File.Exists(txtPgbin.Text + "\\pg_ctl.exe")) return;
+            if (String.IsNullOrEmpty(txtPgdata.Text)) return;
+            toolStripStatusLabel1.Text = "状态：" + runcmd(String.Format("status -D \"{0}\"",txtPgdata.Text)).Split('\r')[0];
+        }
+        public string runcmd(string arge)
+        {
+            try
+            {
+                string result = "";
+                string exe = txtPgbin.Text + "\\pg_ctl.exe";
+                if (!File.Exists(exe))
+                {
+                    throw new ApplicationException("Can not find: " + exe);
+                }
+                StringBuilder sb = new StringBuilder();
+                sb.Append(arge);
+                //sb.Append(" yr");
+                //textBox2.AppendText(sb.ToString() + "\r\n");
+                //txtResult.Text = "";
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = exe;
+                proc.StartInfo.Arguments = sb.ToString();
+                proc.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start();
+                string output = proc.StandardOutput.ReadToEnd() + "\r\n" + proc.StandardError.ReadToEnd();
+                if (String.IsNullOrEmpty(output) == false)
+                    result += output + "\r\n";
+                    //txtResult.AppendText(output + "\r\n");
+                proc.WaitForExit();
+                //proc.BeginOutputReadLine();
+                proc.Close();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            txtCmd.Text =String.Format( "pg_ctl -D \"{0}\" stop -m immediate",txtPgdata.Text);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            result = "";
+            txtResult.Text = "";
+        }
+
+        private void cmbCmd_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbCmd.Text)
+	        {
+                case "help" :
+                    txtCmd.Text = "pg_ctl --help";
+                    break;
+                case "Init":
+                    txtCmd.Text = String.Format("pg_ctl init -D \"{0}\" ", txtPgdata.Text);
+                    break;
+                case "regServer":
+                    txtCmd.Text =String.Format("pg_ctl register -N PostgreSQL -D \"{0}\" ", txtPgdata.Text);
+                    break;
+                case "unregServer":
+                    txtCmd.Text = "pg_ctl unregister -N PostgreSQL";
+                    break;
+		        default:
+                    txtCmd.Text = cmbCmd.Text;
+                    break;
+	        }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ConfigurationSettings.AppSettings[]
+            txtPgbin.Text= ConfigurationManager.AppSettings["pgbin"];
+            txtPgdata.Text = ConfigurationManager.AppSettings["pgdata"];
+        }
+
+        private void txtPgbin_TextChanged(object sender, EventArgs e)
+        {
+            config.AppSettings.Settings["pgbin"].Value = txtPgbin.Text; 
+            config.Save(ConfigurationSaveMode.Modified);  
+            //ConfigurationManager.RefreshSection("appSettings");   
+        }
+
+        private void txtPgdata_TextChanged(object sender, EventArgs e)
+        {
+            config.AppSettings.Settings["pgbin"].Value = txtPgbin.Text;
+            config.Save(ConfigurationSaveMode.Modified);  
+        }
+
+    }
+}
